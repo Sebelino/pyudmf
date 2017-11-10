44ce9c47329b520ade88e0c41892d8dc71927066
Sebastian Olsson <sebelino7@gmail.com>
2017-11-10 18:03:19 +0100

    Finished functional parser with tests.

--- test.py
+++ test.py
@@ -1,8 +1,58 @@
 #!/usr/bin/env python
 
-from udmfscale import parse_udmf
+import pytest
+from .udmfscale import parse_udmf, TranslationUnit, Assignment, Block
 
 
-def test_parse_udmf__empty__empty_list():
-    returned = parse_udmf("")
-    assert returned == []
+@pytest.mark.parametrize("textmap, expected", [
+    ('', TranslationUnit()),
+    ('namespace = "zdoom";', TranslationUnit(Assignment('namespace', 'zdoom'))),
+    ('namespace = "zdoom"; thing {}', TranslationUnit(Assignment('namespace', 'zdoom'), Block('thing', []))),
+    ('namespace = "zdoom"; thing { x = 7; }', TranslationUnit(Assignment('namespace', 'zdoom'),
+                                                              Block('thing', [Assignment('x', '7')]))),
+    ("""
+thing
+{
+x = 608.000;
+y = 256.000;
+angle = 90;
+ambush = true;
+}
+    """,
+     TranslationUnit(
+         Block("thing", [
+             Assignment('x', '608.000'),
+             Assignment('y', '256.000'),
+             Assignment('angle', '90'),
+             Assignment('ambush', 'true'),
+         ])
+     )),
+    ("""
+sidedef
+{
+sector = 7;
+}
+
+sector
+{
+texturefloor = "FLOOR6_2";
+textureceiling = "CEIL3_5";
+heightceiling = 128;
+lightlevel = 156;
+}
+    """,
+     TranslationUnit(
+         Block("sidedef", [
+             Assignment("sector", "7"),
+         ]),
+         Block("sector", [
+             Assignment("texturefloor", "FLOOR6_2"),
+             Assignment("textureceiling", "CEIL3_5"),
+             Assignment("heightceiling", "128"),
+             Assignment("lightlevel", "156"),
+         ])
+     )),
+])
+def test_parse_udmf(textmap, expected):
+    returned = parse_udmf(textmap)
+    assert returned == expected
--- udmfscale.py
+++ udmfscale.py
@@ -1,6 +1,85 @@
 #!/usr/bin/env python
+from abc import abstractmethod, ABCMeta
 
-from pyparsing import Word, alphas, alphanums, Literal, Combine, Optional, nums, QuotedString, OneOrMore, ZeroOrMore
+from pyparsing import Word, alphas, alphanums, Literal, Combine, Optional, nums, QuotedString, ZeroOrMore, \
+    Group
+
+
+class Node(metaclass=ABCMeta):
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and all(a == b for a, b in zip(self, other))
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, ', '.join(repr(c) for c in self))
+
+    @abstractmethod
+    def __getitem__(self, item):
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def group(cls, expr):
+        raise NotImplementedError
+
+
+class TranslationUnit(Node):
+    def __init__(self, *lst):
+        self.global_expr_list = lst
+
+    def __getitem__(self, i):
+        return self.global_expr_list[i]
+
+    @classmethod
+    def group(cls, expr):
+        def group_action(s, l, t):
+            lst = t[0].asList()
+            return cls(*lst)
+        return Group(expr).setParseAction(group_action)
+
+
+class Assignment(Node):
+    def __init__(self, identifier, value):
+        self.identifier = identifier
+        self.value = value
+
+    def __getitem__(self, i):
+        children = [self.identifier, self.value]
+        return children[i]
+
+    @classmethod
+    def group(cls, expr):
+        def group_action(s, l, t):
+            lst = t[0].asList()
+            identifier, _, value, _ = lst
+            return cls(identifier, value)
+
+        return Group(expr).setParseAction(group_action)
+
+    def __str__(self):
+        return "{} = {};".format(self.identifier, self.value)
+
+
+class Block(Node):
+    def __init__(self, identifier: str, expressions: list):
+        self.identifier = identifier
+        self.expressions = expressions
+
+    def __getitem__(self, i):
+        children = [self.identifier, self.expressions]
+        return children[i]
+
+    @classmethod
+    def group(cls, expr):
+        def group_action(s, l, t):
+            lst = t[0].asList()
+            identifier = lst[0]
+            expressions = lst[2:-1]
+            return cls(identifier, expressions)
+        return Group(expr).setParseAction(group_action)
+
+    def __str__(self):
+        expressions_str = " ".join(e for e in self.expressions)
+        return "{} {{}}".format(self.identifier, expressions_str)
 
 
 def parse_udmf(textmap_string: str):
@@ -28,16 +107,17 @@ def parse_udmf(textmap_string: str):
     _integer = Combine(Optional(_plusorminus) + _uinteger)
     _float = Combine(_integer + Optional(Literal('.') + Optional(_uinteger)))
     keyword = Word(alphas)  # [^{}();"'\n\t ]+
-    value = _float | '"' + QuotedString('"') | keyword
+    value = _float | QuotedString('"') | keyword
 
-    assignment_expr = Optional(identifier + '=' + value + ';')
-    expr_list = OneOrMore(assignment_expr)
-    block = identifier + '{' + expr_list + '}'
+    assignment_expr = Assignment.group(identifier + '=' + value + ';')
+    expr_list = ZeroOrMore(assignment_expr)
+    block = Block.group(identifier + '{' + expr_list + '}')
     global_expr = block | assignment_expr
-    global_expr_list = OneOrMore(global_expr)
-    translation_unit = global_expr_list
+    global_expr_list = ZeroOrMore(global_expr)
+    translation_unit = TranslationUnit.group(global_expr_list)
 
-    return translation_unit.parseString(textmap_string)
+    ast = translation_unit.parseString(textmap_string)[0]
+    return ast
 
 
 if __name__ == '__main__':
@@ -48,21 +128,7 @@ if __name__ == '__main__':
     # map = Textmap(textmap_string)
     # print(map)
     textmap_string = """
-thing
-{
-x = 608.000;
-y = 256.000;
-type = 3001;
-angle = 90;
-ambush = true;
-coop = true;
-dm = true;
-single = true;
-skill1 = true;
-skill2 = true;
-skill3 = true;
-skill4 = true;
-skill5 = true;
-}
+namespace = "zdoom";
     """
-    print(parse_udmf(textmap_string))
+    ast = parse_udmf(textmap_string)
+    print(ast)
