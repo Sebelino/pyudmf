5dc8bb79ecae17138da36d362a19d4756be1b368
Sebastian Olsson <sebelino7@gmail.com>
2019-01-04 13:14:15 +0100

    Precondition check for linedefs with zero length

--- pyudmf/model/textmap.py
+++ pyudmf/model/textmap.py
@@ -74,6 +74,7 @@ class Sidedef(object):
 
 class Linedef(object):
     def __init__(self, v1: Vertex, v2: Vertex, sidefront: Sidedef, blocking=False):
+        assert v1 != v2
         self.v1 = v1
         self.v2 = v2
         self.sidefront = sidefront
